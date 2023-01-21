5ec37233f30f75331e6ba11a70545d2085fcef48
shoumitro-cse <shoumitro26@gmail.com>
2023-01-21 18:45:01 +0600

    serializers split fixed

---
+++ accounts/serializers/__init__.py
@@ -0,0 +1,3 @@
+from user import UserSerializer, PermUserSerializer
+from perm import PermissionSerializer, UserPermissionSerializer, GroupSerializer, AddUserWithGroupSerializer
+
--- accounts/serializers/perm.py
+++ accounts/serializers/perm.py
@@ -1,30 +1,6 @@
 from django.contrib.auth.models import Permission, Group
 from accounts.models import User
 from rest_framework import serializers
-from utils.serializers import PermissionModelSerializer
-
-
-class UserSerializer(serializers.ModelSerializer):
-    permission_fields = []
-
-    class Meta:
-        model = User
-        fields = ["username", "password", "first_name", "last_name", "email", "user_type", "mobile", "address"]
-        extra_kwargs = {
-            'password': {
-                'write_only': True
-            }
-        }
-
-    def create(self, validated_data):
-        user = super().create(validated_data)
-        user.set_password(validated_data['password'])
-        user.save()
-        return user
-
-
-class PermUserSerializer(UserSerializer, PermissionModelSerializer):
-    pass
 
 
 class PermissionSerializer(serializers.ModelSerializer):
